e48117f48a8b68bb47db76504ac868b1b7bae409
Alpha Sierra <30448930+Shetty073@users.noreply.github.com>
2020-10-01 22:11:09 +0530

    several changes

--- static/main.js
+++ static/main.js
@@ -1,8 +1,42 @@
+// list page
+let editFlag = false;
+
+$('.todolist-item-complete').on('dblclick', function () {
+    if (editFlag === false) {
+        $(this).css('text-decoration', 'line-through');
+        setTimeout(() => {
+            // TODO (for java ee app): Delete this particular item from database
+
+            // Set the display to none
+            $(this).css('display', 'none');
+        }, 1000);
+    }
+});
+
+function deleteThisTodo() {
+    
+}
 
 function addNewTodoItem() {
     let newTododItem = $('#newtodoitem').val();
 
 
     // send AJAX call for adding this new todo
+    
+}
+
+
+
+// addnewtodo page
+function addAnotherListBtnTextBox() {
+    
+}
+
+function saveTodoList() {
+    
+}
+
+
+function cancel() {
     
 }
